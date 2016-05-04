ad488e70347a3d468e8ebe418779c122578c717e
Alejandro Gonzalez Garcia <alejandro.powerfull@gmail.com>
2016-05-04 19:05:57 +0200

    esqueleto comentarios

--- modulos/perfil/controladorPerfil.js
+++ modulos/perfil/controladorPerfil.js
@@ -1,32 +1,5 @@
 app.controller('controladorPerfil', function(servicioRest, config, $scope, $http, $location, $rootScope, $mdDialog) {
-    document.getElementById("fotoPerfil").setAttribute('src',"http://www.zuliapordentro.com/wp-content/uploads/2016/02/new-google-logo.jpg");
-    $scope.prueba="http://www.zuliapordentro.com/wp-content/uploads/2016/02/new-google-logo.jpg";
-    var reader=new FileReader();
-    $scope.hola=function(){
-        
-        
-        var reader = new FileReader();
-            reader.onload = function (e) {
-                $scope.prueba=e.target.result;
-                //img.src.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
-                document.getElementById("fotoPerfil").setAttribute('src', e.target.result);
-};
-            reader.readAsDataURL(document.getElementById("botonFileReal").files[0]);
-        
-    }
-    $scope.subirFoto = function (){
-        console.log("hola");
-        document.getElementById('botonFileReal').click();//.then({
-            //console.log("hola");
-            //document.getElementById("fotoPerfil").setAttribute("src",$scope.referencia.imagenProyecto)
-        //});
-        
-    }
-    //$scope.prueba="http://www.doralnewsonline.com/doralfinal/wp-content/uploads/2015/10/new-and-old-google-logos.jpg";
-    
-    /*------------------- PROYECTO DEMO DE CHAT CON WEBSOCKETS https://github.com/socketio/socket.io/tree/master/examples/chat -------------------*/
-    
-    var nodeData;
+   var nodeData;
     var operacion;
     $scope.nodoSeleccionado={};
     $scope.nodoSeleccionado.clase="raiz";
@@ -70,6 +43,7 @@ app.controller('controladorPerfil', function(servicioRest, config, $scope, $http
     var elementoSeleccionado=undefined;
     
     
+    $scope.tipos=["OpenSource", "Suscripción", "Licencia"];
     
     $scope.eliminarElem=function(ev, scope){
         if(scope.$modelValue.nodosHijos[0]==null){
@@ -119,6 +93,7 @@ app.controller('controladorPerfil', function(servicioRest, config, $scope, $http
             nombreTecnologia.focus();
         }, 100)
     };
+    
         
     function comprobarArbol(nodos, nombreNodo, encontrado){
         if(nodos.nodosHijos != null){
@@ -188,5 +163,10 @@ app.controller('controladorPerfil', function(servicioRest, config, $scope, $http
             utils.popupInfo('',"El nombre de la tecnologia ya esta en uso");
         }
     };
+    
+      
+
+
+
     
 });
